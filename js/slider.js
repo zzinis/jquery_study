8abec768d24839eea2de31b98c90b22e5d3676be
zzinis <lizzyin@naver.com>
2022-05-11 19:15:31 +0900

    slider js step2

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,46 @@
+let enableClick = true;  
+let len = $(".list li").length; 
+console.log(len); 
+
+
+$(".list li").last().prependTo(".list"); 
+
+$(".list").css({
+    width: 100 * len +"%",
+    height:"100%", 
+    marginLeft:"-100%"
+}); 
+
+$(".list li").css({
+    width: 100 / len +"%",
+    height:"100%", 
+    float:"left"
+});
+
 $(".next").on("click", function(e){
     e.preventDefault(); 
 
-    $(".list").animate({marginLeft:"-200%" },1000, function(){
-        $(".list").css({marginLeft : "-100%"}); 
-        $(".list li").first().appendTo(".list"); 
-    }); 
+    if(enableClick){
+        enableClick = false; 
+        console.log("1");
+        $(".list").animate({marginLeft:"-200%" },1000, function(){
+            $(".list").css({marginLeft : "-100%"}); 
+            $(".list li").first().appendTo(".list");
+            enableClick = true;  
+            console.log("2"); 
+        }); 
+    }
 }); 
 
 $(".prev").on("click", function(e){
     e.preventDefault(); 
 
-    $(".list").animate({ marginLeft: "0%"},1000, function(){
-        $(".list").css({marginLeft :"-100%"}); 
-        $(".list li").last().prependTo(".list"); 
-    });
+    if(enableClick){
+        $(".list").animate({ marginLeft: "0%"},1000, function(){
+            $(".list").css({marginLeft :"-100%"}); 
+            $(".list li").last().prependTo(".list"); 
+            enableClick = true; 
+        });
+        enableClick = false; 
+    }    
 });
