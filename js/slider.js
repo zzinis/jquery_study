23f0dc05207bc2530693b19cc3d8cf5495364e3b
zzinis <lizzyin@naver.com>
2022-05-25 16:56:19 +0900

    slider step5

--- js/slider.js
+++ js/slider.js
@@ -1,74 +1,65 @@
-let enableClick = true;  
+let enableClick = true; 
 let len = $(".list li").length; 
-let timer;  
+let timer;
 
 $(".list li").last().prependTo(".list"); 
 
 $(".list").css({
     width: 100 * len +"%",
-    height:"100%", 
     marginLeft:"-100%"
 }); 
 
 $(".list li").css({
-    width: 100 / len +"%",
-    height:"100%", 
+    width: 100 / len +"%", 
     float:"left"
 });
+
 
 $(".next").on("click", function(e){
     e.preventDefault(); 
 
     if(enableClick){
+        enableClick = false;
+
+        $(".list").animate({marginLeft :"-200%"},1000, function(){
+            $(".list").css({marginLeft: "-100%"}); 
+            $(".list li").first().appendTo(".list"); 
+            enableClick = true; 
+        });
+    }
+});
+
+$(".prev").on("click", function(e){
+    e.preventDefault(); 
+    if(enableClick){
         enableClick = false; 
-        console.log("1");
-        $(".list").animate({marginLeft:"-200%" },1000, function(){
-            $(".list").css({marginLeft : "-100%"}); 
-            $(".list li").first().appendTo(".list");
-            enableClick = true;  
-            console.log("2"); 
+        $(".list").animate({marginLeft:"0%"},1000, function(){
+            $(".list").css({marginLeft:"-100%"}); 
+            $(".list li").last().prependTo(".list"); 
+            enableClick = true; 
         }); 
     }
 }); 
 
-$(".prev").on("click", function(e){
-    e.preventDefault(); 
-
-    if(enableClick){
-        $(".list").animate({ marginLeft: "0%"},1000, function(){
-            $(".list").css({marginLeft :"-100%"}); 
-            $(".list li").last().prependTo(".list"); 
-            enableClick = true; 
-        });
-        enableClick = false; 
-    }    
-});
 
 $(".start").on("click", function(e){
     e.preventDefault(); 
 
-    let isOn = $(this).hasClass("on"); 
-    if(isOn) return; 
-
     timer = setInterval(function(){
-        $(".list").animate({marginLeft:"-200%" },1000, function(){
-            $(".list").css({marginLeft : "-100%"}); 
-            $(".list li").first().appendTo(".list");          
-        }); 
-    },2000);  
+        $(".list").animate({marginLeft :"-200%"},1000, function(){
+            $(".list").css({marginLeft: "-100%"}); 
+            $(".list li").first().appendTo(".list"); 
+        });
+    },2000);
     
     $(".start").addClass("on"); 
     $(".stop").removeClass("on"); 
-});
+}); 
 
 $(".stop").on("click", function(e){
-    e.preventDefault();
-    
-    let isOn = $(this).hasClass("on"); 
-    if(isOn) return; 
-    
+    e.preventDefault(); 
     clearInterval(timer); 
 
-    $(".stop").addClass("on"); 
     $(".start").removeClass("on"); 
-}); 
+    $(".stop").addClass("on");
+});
