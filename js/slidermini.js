a5a3ad5c5c471d53465b8a98585489e7da9ed1a1
zzinis <lizzyin@naver.com>
2022-05-30 19:48:49 +0900

    slidermini js 수정

--- js/slidermini.js
+++ js/slidermini.js
@@ -1,14 +1,29 @@
 const $slider = $("#slider"); 
 const $panel = $slider.find(".panel"); 
 const $btns = $slider.find(".navi li"); 
+const $circle = $slider.find("#circle"); 
+let enableClick = true; 
+let speed = 1000; 
 
 $btns.on("click", function(e){
     e.preventDefault(); 
+
+    let isOn = $(this).hasClass("on"); 
+    if(isOn) return; 
     var i = $(this).index(); 
 
-    $panel.animate({ marginLeft : (-100 * i) +"%"}, 1000); 
+    if(enableClick){
+        $panel.animate({ marginLeft : (-100 * i) +"%"}, speed, function(){
+            enableClick = true; 
+        }); 
 
-    $btns.removeClass("on"); 
-    $btns.eq(i).addClass("on"); 
+        $btns.removeClass("on"); 
+        $btns.eq(i).addClass("on"); 
+
+        $circle.removeClass(); 
+        $circle.addClass("rot"+i); 
+
+        enableClick = false; 
+    }
 }); 
 
